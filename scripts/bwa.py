46b325bcdeb8f786e59df0727bc101d5e9ccc4d5
sushantpande <we.sushant@gmail.com>
2019-06-04 23:05:21 +0530

    Call to utility and conn

--- scripts/bwa.py
+++ scripts/bwa.py
@@ -1,138 +1,108 @@
 import subprocess
 from subprocess import PIPE
-import redis
 import os
 import pysam
-import pika
+import sys
 import time
 import glob
+import bwa_split
+import conn
+import utility
+import yaml
 
-REDIS_HOST = os.environ['REDIS_HOST']
-TASK_ID = os.environ['TASK_ID']
-RUN_ID = os.environ['RUN_ID']
-DATA_DIR = os.environ['DATA_DIR']
-AMQP_SERVER = os.environ['AMQP_SERVER']
-AMQP_PORT = os.environ['AMQP_PORT']
-AMQP_USER = os.environ['AMQP_USER']
-AMQP_PWD = os.environ['AMQP_PWD']
-INPUT_FILE = os.environ['INPUT_FILE']
-REFFILE = os.environ['REF_FILE']
-split = int(os.environ['SPLIT'])
-split_size = int(os.environ['SPLIT_SIZE'])
 
-print  ('REDIS_HOST: ' +  REDIS_HOST)
-print  ('TASK_ID: ' + TASK_ID)
-print  ('RUN_ID: ' + RUN_ID)
+redis_conn = None
+process_from_queue = False
 
-input = os.path.join(DATA_DIR, INPUT_FILE)
-output_dir = os.path.join(DATA_DIR, RUN_ID, TASK_ID)
-output = os.path.join(output_dir, 'bwaoutput.bam')
-ref = os.path.join(DATA_DIR, REFFILE)
-print  ('INPUT: ' + input)
-print  ('OUTPUT: ' + output)
-print  ('REF: ' + ref)
+'''Get input file'''
+input_file = utility.get_input_file()
+in_queue = None
 
-try:
-    os.makedirs(output_dir)
-except FileExistsError:
-    pass
+if not input_file:
+    print ("No input file for Job, looking for input queue")
+
+    '''Get input queue'''
+    in_queue_str = utility.get_env_param('IN_QUEUE')
+    in_queue_list = in_queue_str.split()
+    in_queue = None
+
+    if not in_queue_list:
+        print ("Found no input queue for job")
+        sys.exit(os.EX_SOFTWARE)
+    else:
+        in_queue = in_queue_list[0]
+        process_from_queue = True
+
+
+'''Get output dir'''
+output_dir = utility.get_output_dir()
+
+'''Get output file'''
+output_file = os.path.join(output_dir, "bwaoutput.bam")
+
+'''Get Reference file'''
+ref = utility.get_ref_file()
+
+'''Get AMQP connection'''
+amqp_conn = conn.get_amqp_conn()
 
 cmd = 'bwa mem -R "@RG\\tID:foo\\tLB:bar\\tPL:illumina\\tPU:illumina\\tSM:SAMPLE" ' + ref + ' ' +\
-      input + ' | samtools sort > ' + output + ' && samtools index ' + output
+      input_file + ' | samtools sort > ' + output_file + ' && samtools index ' + output_file
+#cmd = 'bwa mem -R "@RG\\tID:foo\\tLB:bar\\tPL:illumina\\tPU:illumina\\tSM:ERR000589" ' + ref + ' ' +\
+#      input_file + ' | samtools sort > ' + output_file + ' && samtools index ' + output_file
+
+print ("%s %s" %("cmd:" , cmd))
 
 completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
+print ("BWA mem output: %s" %(completedProc.stdout))
+print ("BWA mem error: %s" %(completedProc.stderr))
 
-print ('BWA container creation completed with status code:' + ' ' + str(completedProc.returncode))
-
-
-
-#Update jobs in the pipeline
-
-credentials = pika.PlainCredentials(AMQP_USER, AMQP_PWD)
-parameters = pika.ConnectionParameters(AMQP_SERVER, AMQP_PORT, '/', credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-queue = ("%s.%s" %(RUN_ID, TASK_ID))
-channel.queue_declare(queue=queue)
-
-split_count = 0
+output_file_list = []
+split = int(utility.get_env_param('BWA_SPLIT_OUTPUT'))
+split_size = int(utility.get_env_param('BWA_SPLIT_OUTPUT_SIZE'))
 
 if split == 1:
-    split_output_dir = os.path.join(DATA_DIR, RUN_ID, TASK_ID, "split")
-
-    try:
-        os.makedirs(split_output_dir)
-    except FileExistsError:
-        pass
+    split_output_dir = utility.get_split_dir() 
 
     if split_size:
-        lc = 500000
-        cmd = "samtools view " + output + " | wc -l"
-        print ("cmd: %s" %cmd)
-        completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
-
-        per_file_count = int(int(completedProc.stdout)/split_size)
-        if per_file_count < 1:
-            print("Cannot split file: Reduce split size")
-            lc = int(completedProc.stdout)
-        else:
-            lc = per_file_count
-        print ("Lines per file would be ~ %s" %(lc))
-        header_file = os.path.join(split_output_dir, "header")
-        glob_split_output_dir = os.path.join(split_output_dir, "*")
-        cmd = "samtools view -H " + output + " > " + header_file
-        print ("cmd: %s" %cmd)
-        completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
-        split_prefix = os.path.join(split_output_dir,"bwaoutput")
-        cmd = "samtools view " + output + " | split - " + split_prefix + " -l " + str(lc) + " --filter='cat " + header_file + " - | samtools view -b - > $FILE.bam' && rm " + header_file     
-        print ("cmd: %s" %cmd)
-        completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
-        file_list = glob.glob(glob_split_output_dir)
-        for file in file_list:
-            cmd = "samtools index " + file
-            print ("cmd: %s" %cmd)
-            completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
-            channel.basic_publish(exchange='', routing_key=queue, body=file)
-        split_count = len(file_list)
+        output_file_list = bwa_split.split_by_file_count(output_file, split_output_dir, split_size)
     else:    
-        cmd = "samtools view -H" + " " + output + " | cut -f2 | grep '^SN:' | sed s'/SN://'"
-        completedProc = subprocess.run([cmd, "/dev/null"], shell=True, stdout=PIPE, stderr=PIPE)
-        samfile = pysam.AlignmentFile(output, "rb")
-
-        for chr in (completedProc.stdout.decode('utf-8').split("\n")):
-            if not chr:
-                continue
-            print ("Fetching chr: " + chr)
-            reads = samfile.fetch(chr)
-            outfile = None
-            if reads:
-                filename = chr + "_split.bam"
-                filepath = os.path.join(output_dir, filename)
-                outfile = pysam.AlignmentFile(filepath, "wb", template=samfile)
-            for read in reads:
-                outfile.write(read)
-            outfile.close()
-            pysam.index(filepath)
-            channel.basic_publish(exchange='', routing_key=queue, body=filepath)
-            print(" [x] Sent %r" % filepath)
-            split_count = split_count + 1
-
-        samfile.close()
+        output_file_list = bwa_split.split_by_chr(output_file, split_output_dir)
 else:
-    split_count = 1
-    channel.basic_publish(exchange='', routing_key=queue, body=output)
+    output_file_list.append(output)
 
-#while(True):
-#    time.sleep(300)
+out_queue = utility.get_env_param('OUT_QUEUE')
 
+channel_out = amqp_conn.channel()
+channel_out.queue_declare(queue=out_queue)
 
-r = redis.Redis(host=REDIS_HOST)
-update_key = RUN_ID + "." + TASK_ID
-r.set(update_key, completedProc.returncode)
-print ('updated key: ' + update_key)
-split_key = update_key + ".split"
-r.set(split_key, split_count)
-print ('updated key: ' + split_key)
-queue_key = ("%s.queue" %(RUN_ID))
-r.set(queue_key, queue) 
-print ('Queue is: %s' %(queue))
+for output_file in output_file_list:
+    status = conn.publish_to_amqp(channel_out, routing_key=out_queue, body=output_file)
+    if not status:
+        print ("Cannot publish to AMQP, continuing to process, refer to meta\
+                dir for manul update of AMQP")
+
+'''Update meta file'''
+meta = {}
+meta.update({'OUTPUT_DIR': output_dir})
+meta.update({'OUT_QUEUE': out_queue})
+meta.update({'IN_QUEUE': in_queue})
+meta.update({'OUTPUT_FILE_LIST': output_file_list})
+meta.update({'OUTPUT_FILE_COUNT': len(output_file_list)})
+meta_file = utility.get_meta_file()
+print ("Will write meta info. to file (%s)" %(meta_file))
+
+with open(meta_file, 'w+') as fh:
+    yaml.dump(meta, fh, default_flow_style=False)
+
+'''Update Redis server'''
+redis_conn = conn.get_redis_conn()
+key = ("%s.%s.%s" %(utility.run_id, utility.task_id, 'OUTPUT_FILE_COUNT'))
+if redis_conn:
+    status = conn.update_to_redis(redis_conn, key, len(output_file_list))
+    if not status:
+        print ("Failed to update redis key (%s), val (%s)" %(key, len(output_file_list)))
+else:
+    print ("Failed to update redis key (%s), val (%s)" %(key, len(output_file_list)))
+
+sys.exit(os.EX_OK)
